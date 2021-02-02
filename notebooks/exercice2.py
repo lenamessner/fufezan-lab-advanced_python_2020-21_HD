30fc7c1374a7a24fae555f50ba35a5268814b28f
lenamessner <lena.messner@stud.uni-heidelberg.de>
2021-02-02 20:34:25 +0100

    added code that extracts csv-file

--- notebooks/exercice2.py
+++ notebooks/exercice2.py
@@ -10,16 +10,19 @@ with open("../data/uniprot-archae+AND+reviewed_yes+AND+Methanococcus+maripaludis
             all_sequences += row.replace("\n", "")  #.replace, damit all_sequences alles in einer Zeile
 
 counter_dict = dict(Counter(all_sequences))
-print(counter_dict.keys())
-print(counter_dict.values())
 
-# with open("../human.csv", "w") as output:
-#     aap_writer = csv.DictWriter(output, fieldnames=["aa", "count"])
-#     aap_writer.writeheader()
-#     for key, value in counter_dict.items():
-#         aap_writer.writerow({key: value})
+# for key, value in counter_dict.items():
+#     print(key)
+#     print(value)
+    # gibt abwechselnd Buchstabe und Zahl -> wie erwartet
 
-as_names = []
+with open("../human.csv", "w") as output:
+    csv_writer = csv.DictWriter(output, fieldnames=["aa", "count"], delimiter=",")
+    csv_writer.writeheader()
+    for key, value in counter_dict.items():
+        csv_writer.writerow({'aa': key, 'count': value})
+
+as_names = []   # geht auch einfach als list(counter_dict.keys())
 for key in counter_dict.keys():
     as_names += key
 print(as_names)
@@ -36,8 +39,3 @@ plt.xlabel("amino acid")
 plt.title("Histogram of amino acids in Methanococcus maripaludis")
 # plt.show()
 plt.savefig("../results/hist_mmaripaludis_messner.pdf")
-
-# with open("../test.csv", "w") as output:
-#     aap_writer = csv.DictWriter(output, fieldnames=["Name", "3-letter code"], extrasaction="ignore")
-#     aap_writer.writeheader()
-#     aap_writer.writerow({"Name": "Alanine", "3-letter code": "Ala", "1-letter code": "A"})
