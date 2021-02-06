708727b38321362957f2ed5f1e22144f79ff5c89
lenamessner <lena.messner@stud.uni-heidelberg.de>
2021-02-06 11:28:24 +0100

    added docstrings

--- exercice2/exercice2.py
+++ exercice2/exercice2.py
@@ -4,6 +4,15 @@ from matplotlib import pyplot as plt
 
 
 def extract_sequence(fastafile):
+    """
+    takes fastafile with headers, returns only aa sequence as string
+    Args:
+        fastafile: fastafile with different genome sequences from one organism
+
+    Returns:
+        all_sequences: string containing all sequences of fastafile without headers
+
+    """
     with open(dataset) as fastafile:
         all_sequences = ""
         for row in fastafile:
@@ -13,9 +22,18 @@ def extract_sequence(fastafile):
 
 
 def results_as_csv(ordered_dict=None, name_of_organism=""):
+    """
+    stores the results in a csv-file
+    Args:
+        ordered_dict: dict that maps aa to corresponding count
+        name_of_organism: name of the organism
+
+    Returns:
+
+    """
     if ordered_dict is None:
         ordered_dict = {}
-    with open("results_exercice2/csv__orderd.csv", "w") as output:
+    with open(f"results_exercice2/csv_{name_of_organism}_orderd.csv", "w") as output:
         csv_writer = csv.DictWriter(output, fieldnames=["aa", "count"], delimiter=",")
         csv_writer.writeheader()
         for key, value in ordered_dict.items():
@@ -24,6 +42,15 @@ def results_as_csv(ordered_dict=None, name_of_organism=""):
 
 
 def plot_results(ordered_dict=None, name_of_organism=""):
+    """
+    plots given ordered dict: counts-aa-plot
+    Args:
+        ordered_dict: dict containing aa with corresponding counts
+        name_of_organism: name of organism
+
+    Returns:
+
+    """
     if ordered_dict is None:
         ordered_dict = {}
     as_names = []  # geht auch einfach als list(counter_dict.keys())
@@ -60,9 +87,4 @@ if __name__ == '__main__':
     # gibt abwechselnd Buchstabe und Zahl -> wie erwartet
 
     plot_results(ordered_dict, "mmaripaludis")
-
-    with open("results_exercice2/csv_mmaripaludis_ordered.csv", "w") as output:
-        csv_writer = csv.DictWriter(output, fieldnames=["aa", "count"], delimiter=",")
-        csv_writer.writeheader()
-        for key, value in ordered_dict.items():
-            csv_writer.writerow({'aa': key, 'count': value})
+    results_as_csv(ordered_dict, "mmaripulaudis")
