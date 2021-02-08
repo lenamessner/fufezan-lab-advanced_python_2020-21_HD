365f39619905bf9d436c73d89c402606e8bda3ed
lenamessner <lena.messner@stud.uni-heidelberg.de>
2021-02-08 22:14:58 +0100

    added test for plotting function

--- tests/test_protein.py
+++ tests/test_protein.py
@@ -1,6 +1,6 @@
 import exercice4.protein as ep
 import pandas as pd
-
+import plotly.graph_objects as go
 
 def test_get_data():
     testprotein = ep.Protein("P32249", lookup={})
@@ -49,4 +49,53 @@ def test_map_2():
     testprotein = ep.Protein("P32249", lookup=lookup_dict)
     testprotein.sequence = "ACEGCEE"
     population = testprotein.map("population", 2)
-    assert population == [(200.4+143.5)/2, (143.5+1357.0)/2, (1357.0+52.98)/2, (52.98+143.5)/2, (143.5+1357.0)/2, (1357.0+1357.0)/2]
+    assert population == [(200.4 + 143.5) / 2, (143.5 + 1357.0) / 2, (1357.0 + 52.98) / 2, (52.98 + 143.5) / 2,
+                          (143.5 + 1357.0) / 2, (1357.0 + 1357.0) / 2]
+
+
+def test_plot_values():
+    fig = ep.plot_values(value_list=[1, 1.5, 8, -0.3, 5.3, -1.2, 0.1], seq="ACEGCEE", title="test")
+    data = [
+        go.Bar(
+            x=[0, 1, 2, 3, 4, 5, 6],
+            y=[1, 1.5, 8, -0.3, 5.3, -1.2, 0.1],
+            marker_color="rgba(168, 0, 0, 1)",
+        )
+    ]
+
+    layout = {
+        "title": {
+            "text": "test",
+            "font_size": 30,
+        },
+        "plot_bgcolor": "rgba(0, 0, 0, 0.1)",  # Hintergrundfarbe
+        "xaxis":{
+            "color": "rgba(0, 0, 0, 1)",
+            "title": {
+                "text": "starting position of sliding window",
+                "font_size": 20,
+                "font_family": "Courier"
+            },
+            "showline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": False
+        },
+        "yaxis": {
+            "showgrid": True,
+            "gridwidth": 1,
+            "gridcolor": "rgba(0, 0, 0, 0.2)",
+            "color": "rgba(0,0,0,1)",
+            "ticks": "outside",
+            "title": {
+                "text": "hydopathy",
+                "font_size": 20,
+                "font_family": "Courier",
+            },
+            "showline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": True,  # sagt ob rechts auch ein Strich ist
+        }
+    }
+    assert fig == go.Figure(data=data, layout=layout)
