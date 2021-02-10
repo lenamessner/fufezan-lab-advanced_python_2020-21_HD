1626edf2b0fe07c955408ace10880470ca2ecffa
lenamessner <lena.messner@stud.uni-heidelberg.de>
2021-02-10 23:28:07 +0100

    added code to get incidence per cont and year

--- exercice7/exercice7.py
+++ exercice7/exercice7.py
@@ -75,38 +75,104 @@ cdf_clean_sort = cdf_clean.sort_values("date")  # cdf_clean_sort hat au keine ne
 # x["inc_diff"] = x["14d-incidence"].diff()
 # x["inc_diff"].fillna(0, inplace=True)
 
-a = np.empty((0, 4))
-for continent, cont_grp in cdf_clean_sort.groupby(["continent"]):
-    # print(continent)
-    for country, country_grp in cont_grp.groupby(["countries"]):
-        # print(country)
-        diffs = country_grp["14d-incidence"].diff().fillna(0)
-        min_dif = min(diffs)
-        max_dif = max(diffs)
-        a = np.append(a, [[continent, country, min_dif, max_dif]], axis=0)
 
-a_pandas = pd.DataFrame(a)
-a_pandas.columns=["continent", "country", "min_difs", "max_difs"]
-a_pandas["min_difs"] = pd.to_numeric(a_pandas["min_difs"])
-a_pandas["max_difs"] = pd.to_numeric(a_pandas["max_difs"])
-country_lowest_diff = a_pandas.loc[a_pandas["min_difs"].idxmin(), "country"]
-min_dif_value = a_pandas["min_difs"].min()
-print(f"The country with the most drastic decrease of the 14d-incidence is: {country_lowest_diff} (value: {min_dif_value})")
-country_highest_diff = a_pandas.loc[a_pandas["max_difs"].idxmax(), "country"]
-max_dif_value = a_pandas["max_difs"].max()
-print(f"The country with the most drastic increase of the 14d-incidence is: {country_highest_diff} (Value: {max_dif_value})")
+def give_deltainc_percontinent(cdf_clean_sort):
+    """
 
-final = np.empty((0, 5))
-for continent, grp_continent in a_pandas.groupby("continent"):
-    index_min = grp_continent["min_difs"].idxmin()
-    index_max = grp_continent["max_difs"].idxmax()
-    final = np.append(final, [[continent,
-                              a_pandas.loc[index_min, "country"],
-                              a_pandas.loc[index_min, "min_difs"],
-                              a_pandas.loc[index_max, "country"],
-                              a_pandas.loc[index_max, "max_difs"]]], axis = 0)
+    Args:
+        cdf_clean_sort: pandas dataframe
 
-final_pandas = pd.DataFrame(final)
+    Returns:
+        final_pandas: df containing countries with highest/lowest increase per continent
+
+    """
+    a = np.empty((0, 4))
+    for continent, cont_grp in cdf_clean_sort.groupby(["continent"]):
+        # print(continent)
+        for country, country_grp in cont_grp.groupby(["countries"]):
+            # print(country)
+            diffs = country_grp["14d-incidence"].diff().fillna(0)
+            min_dif = min(diffs)
+            max_dif = max(diffs)
+            a = np.append(a, [[continent, country, min_dif, max_dif]], axis=0)
+
+    a_pandas = pd.DataFrame(a)
+    a_pandas.columns=["continent", "country", "min_difs", "max_difs"]
+    a_pandas["min_difs"] = pd.to_numeric(a_pandas["min_difs"])
+    a_pandas["max_difs"] = pd.to_numeric(a_pandas["max_difs"])
+    country_lowest_diff = a_pandas.loc[a_pandas["min_difs"].idxmin(), "country"]
+    min_dif_value = a_pandas["min_difs"].min()
+    print(f"The country with the most drastic decrease of the 14d-incidence is: {country_lowest_diff} (value: {min_dif_value})")
+    country_highest_diff = a_pandas.loc[a_pandas["max_difs"].idxmax(), "country"]
+    max_dif_value = a_pandas["max_difs"].max()
+    print(f"The country with the most drastic increase of the 14d-incidence is: {country_highest_diff} (Value: {max_dif_value})")
+
+    final = np.empty((0, 5))
+    for continent, grp_continent in a_pandas.groupby("continent"):
+        index_min = grp_continent["min_difs"].idxmin()
+        index_max = grp_continent["max_difs"].idxmax()
+        final = np.append(final, [[continent,
+                                  a_pandas.loc[index_min, "country"],
+                                  a_pandas.loc[index_min, "min_difs"],
+                                  a_pandas.loc[index_max, "country"],
+                                  a_pandas.loc[index_max, "max_difs"]]], axis=0)
+
+    final_pandas = pd.DataFrame(final)
+    final_pandas.columns = ["continent", "min_country", "min_value", "max_country", "max_value"]
+    return final_pandas
+
+def give_deltainc_percontinent_andyear(cdf_clean_sort):
+    """
+
+    Args:
+        cdf_clean_sort: pandas dataframe
+
+    Returns:
+        final_pandas: df containing countries with highest/lowest increase per continent and year
+
+    """
+    a = np.empty((0, 5))
+    for continent, cont_grp in cdf_clean_sort.groupby(["continent"]):
+        # print(continent)
+        for country, country_grp in cont_grp.groupby(["countries"]):
+            # print(country)
+            for year, year_grp in country_grp.groupby(["year"]):
+                diffs = year_grp["14d-incidence"].diff().fillna(0)
+                min_dif = min(diffs)
+                max_dif = max(diffs)
+                a = np.append(a, [[continent, country, min_dif, max_dif, year]], axis=0)
+
+    a_pandas = pd.DataFrame(a)
+    a_pandas.columns=["continent", "country", "min_difs", "max_difs", "year"]
+    a_pandas["min_difs"] = pd.to_numeric(a_pandas["min_difs"])
+    a_pandas["max_difs"] = pd.to_numeric(a_pandas["max_difs"])
+
+    final = np.empty((0, 6))
+    for continent, grp_continent in a_pandas.groupby("continent"):
+        for year, year_grp in grp_continent.groupby("year"):
+            index_min = year_grp["min_difs"].idxmin()
+            index_max = year_grp["max_difs"].idxmax()
+            final = np.append(final, [[year, continent,
+                                      a_pandas.loc[index_min, "country"],
+                                      a_pandas.loc[index_min, "min_difs"],
+                                      a_pandas.loc[index_max, "country"],
+                                      a_pandas.loc[index_max, "max_difs"]]], axis=0)
+
+    final_pandas = pd.DataFrame(final)
+    final_pandas.columns = ["year", "continent", "min_country", "min_value", "max_country", "max_value"]
+    final_pandas["min_value"] = pd.to_numeric(final_pandas["min_value"])
+    final_pandas["max_value"] = pd.to_numeric(final_pandas["max_value"])
+    final_pandas["year"] = pd.to_numeric(final_pandas["year"])
+    country_lowest_diff = final_pandas.loc[final_pandas["min_value"].idxmin(), "min_country"]
+    min_dif_value = final_pandas["min_value"].min()
+    print(
+        f"The country with the most drastic decrease of the 14d-incidence is: {country_lowest_diff} (value: {min_dif_value})")
+    country_highest_diff = final_pandas.loc[final_pandas["max_value"].idxmax(), "max_country"]
+    max_dif_value = final_pandas["max_value"].max()
+    print(
+        f"The country with the most drastic increase of the 14d-incidence is: {country_highest_diff} (Value: {max_dif_value})")
+
+    return final_pandas
 
 # plotting
 # data = [
@@ -118,3 +184,6 @@ final_pandas = pd.DataFrame(final)
 # ]
 # fig = go.Figure(data=data)
 # fig.show()
+
+if __name__ == '__main__':
+    print(give_deltainc_percontinent(cdf_clean_sort))
