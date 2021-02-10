12168b621970e8acec1f2728e948f4e420810d74
lenamessner <lena.messner@stud.uni-heidelberg.de>
2021-02-10 18:37:08 +0100

    added code for calculating difference in incidence

--- exercice7/exercice7.py
+++ exercice7/exercice7.py
@@ -3,6 +3,7 @@ import numpy as np
 import ssl
 import json
 import urllib.request
+import plotly.graph_objects as go
 
 # get COVID data from ECDC
 covid_url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
@@ -54,19 +55,53 @@ for column in ['deaths_weekly', 'cases_weekly', '14d-incidence']:
 
 # drop dates in future
 mask_date = cdf_clean["date"] < cdf.loc[0, "date"].now()
-cdf_clean = cdf_clean[mask_date]
-
+cdf_clean = cdf_clean[mask_date]  # cdf_clean hat keine neuen Indices! Indices wie bei cdf!
 
 # Identify those countries (grouped by continent) which showed the most drastic increase most drastic and decrease of
 # the 14d-incidence within the different years since recording. Visualize intuitively!
 
-cdf_clean_sort = cdf_clean.sort_values("date")
+cdf_clean_sort = cdf_clean.sort_values("date")  # cdf_clean_sort hat au keine neuen Indices! Indices wie bei cdf!
 
-# group by continents and countries
-grouped = cdf_clean_sort.groupby(["continent", "countries"])
-grouped["14d-incidence"].idxmax()  # gibt: continent->country->idx aus cdf_clean für maximale 14d-incidence aus dem Land
-# calculate difference
+# erster Versuch
+# # group by continents and countries
+# grouped = cdf_clean_sort.groupby(["continent", "countries"])
+# grouped["14d-incidence"].idxmax()  # gibt: continent->country->idx aus cdf für maximale 14d-incidence aus dem Land
+# # calculate difference
+#
+# x = grouped.get_group(("Africa", "Algeria"))
+# # Daten nach Datum sortieren, dann Differenz zu vorherigem Eintrag pd[col].diff() dann noch nan=0
+# x["inc_diff"] = x["14d-incidence"].diff()
+# x["inc_diff"].fillna(0, inplace=True)
+
+a = np.empty((0, 3))
+for continent, cont_grp in cdf_clean_sort.groupby(["continent"]):
+    # print(continent)
+    for country, country_grp in cont_grp.groupby(["countries"]):
+        # print(country)
+        diffs = country_grp["14d-incidence"].diff().fillna(0)
+        min_dif = min(diffs)
+        max_dif = max(diffs)
+        a = np.append(a, [[country, min_dif, max_dif]], axis = 0)
+
+a_pandas = pd.DataFrame(a)
+a_pandas.columns=["country", "min_difs", "max_difs"]
+a_pandas["min_difs"] = pd.to_numeric(a_pandas["min_difs"])
+a_pandas["max_difs"] = pd.to_numeric(a_pandas["max_difs"])
+country_lowest_diff = a_pandas.loc[a_pandas["min_difs"].idxmin(), "country"]
+min_dif_value = a_pandas["min_difs"].min()
+print(f"The country with the most drastic decrease of the 14d-incidence is: {country_lowest_diff} (value: {min_dif_value})")
+country_highest_diff = a_pandas.loc[a_pandas["max_difs"].idxmax(), "country"]
+max_dif_value = a_pandas["max_difs"].max()
+print(f"The country with the most drastic increase of the 14d-incidence is: {country_highest_diff} (Value: {max_dif_value})")
 
 
-# groupby["continent", "countries"]
-# Daten nach Datum sortieren, dann Differenz zu vorherigem Eintrag pd[col].diff["14d-incidence"] dann noch nan=0
+# plotting
+# data = [
+#     go.Bar(
+#         x=country_names,
+#         y=yvalues,
+#         marker_color="rgba(168, 0, 0, 1)",
+#     )
+# ]
+# fig = go.Figure(data=data)
+# fig.show()
